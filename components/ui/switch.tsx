a4ffac4996ba15c5c3ca125cdd26d0b488dc0e51
Gunabh Sharan <gunabhsharan.25@gmail.com>
2026-05-03 16:04:46 +0530

    fix: update SwitchProps to inherit from ButtonHTMLAttributes instead of InputHTMLAttributes

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { cn } from '@/lib/utils'
 
-interface SwitchProps extends Omit<React.InputHTMLAttributes<HTMLButtonElement>, 'onChange'> {
+interface SwitchProps extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'onChange'> {
   checked?: boolean
   onCheckedChange?: (checked: boolean) => void
 }
